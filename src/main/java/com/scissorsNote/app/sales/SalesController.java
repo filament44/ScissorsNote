3f1de9864ecf075e74828a25fe8f0ffe6a3c668a
filament44 <concier11@gmail.com>
2020-06-11 17:21:56 +0900

    確認ページまで。テータベースへの格納はまだ

--- src/main/java/com/scissorsNote/app/sales/SalesController.java
+++ src/main/java/com/scissorsNote/app/sales/SalesController.java
@@ -3,6 +3,7 @@ package com.scissorsNote.app.sales;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +14,12 @@ public class SalesController {
 	public String form(Model model) {
 		model.addAttribute("title", "Sales Form");
 		return "sales/form";
+	}
+	
+	@PostMapping("/confirm")
+	public String confirm(SalesForm salesForm, Model model) {
+		model.addAttribute("title", "Confirm Page");
+		return "sales/confirm";
 	}
 
 }
---
+++ src/main/java/com/scissorsNote/app/sales/SalesForm.java
@@ -0,0 +1,45 @@
+package com.scissorsNote.app.sales;
+
+import java.util.Date;
+
+public class SalesForm {
+//	private Date date;
+	private String name;
+	private String gender;
+	private int age;
+	private int price;
+	
+	public SalesForm() {}
+	
+//	public Date getDate() {
+//		return date;
+//	}
+//	public void setDate(Date date) {
+//		this.date = date;
+//	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
